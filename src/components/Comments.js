d71bf4e4daabc857a21a57a6dac9d3bb2c2c2e07
bhavya-tech <bhavya.p@ahduni.edu.in>
2022-10-03 23:11:06 +0530

    comments functional

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,55 +1,59 @@
-function CommentBox() {
+import { CommentsData } from "../data/CommentsData";
+
+function ReplyBox(name, date, comment) {
     return (
         <div class="p-6 m-3 shadow-lg rounded-lg bg-gray-100 text-gray-700">
             <section>
                 <img src="https://picsum.photos/200/300" alt="profile" class="rounded-full h-12 w-12 float-left mr-4" />
-                <h3 class="font-bold">John Doe</h3>
-                <p class="text-sm">Posted on 1/1/2020</p>
+                <h3 class="font-bold">{name}</h3>
+                <p class="text-sm">Posted on {date}</p>
             </section>
-        
+
             <hr class="my-6 border-gray-300" />
             <p>
-                It uses utility classes for typography and spacing to space content out within the larger
-                container.
+                {comment}
             </p>
-            
+        </div>
+    );
+}
+
+function CommentBox(name, date, comment, replies) {
+    const repliesList = replies ? replies.map((reply) => ReplyBox(reply.name, reply.posted_on, reply.comment)) : null;
+
+    return (
+        <div class="p-6 m-3 shadow-lg rounded-lg bg-gray-100 text-gray-700">
+            <section>
+                <img src="https://picsum.photos/200/300" alt="profile" class="rounded-full h-12 w-12 float-left mr-4" />
+                <h3 class="font-bold">{name}</h3>
+                <p class="text-sm">Posted on {date}</p>
+            </section>
+
+            <hr class="my-6 border-gray-300" />
+            <p>
+                {comment}
+            </p>
+
             <div class="flex justify-end">
                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
                     Reply
                 </button>
             </div>
 
-            {/* Replies */ }
+            {/* Replies */}
             <h3 class="font-bold mt-6">Replies</h3>
-            <div class="p-6 m-3 shadow-lg rounded-lg bg-gray-100 text-gray-700">
-                <section>
-                    <img src="https://picsum.photos/200/300" alt="profile" class="rounded-full h-12 w-12 float-left mr-4" />
-                    <h3 class="font-bold">John Doe</h3>
-                    <p class="text-sm">Posted on 1/1/2020</p>
-                </section>
-            
-                <hr class="my-6 border-gray-300" />
-                <p>
-                    It uses utility classes for typography and spacing to space content out within the larger
-                    container.
-                </p>
-                
-                <div class="flex justify-end">
-                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                        Reply
-                    </button>
-                </div>
-            </div>
+            {repliesList}
         </div>
     )
 }
 
 export default function Comments() {
+    const commentData = CommentsData;
+    console.log(commentData);
+    const comments = commentData.map((comment) => CommentBox(comment.name, comment.posted_on, comment.comment, comment.replies));
     return (
         <div>
-            
-            <CommentBox />
-            <CommentBox />
+            <h1 class="text-4xl font-bold">Comments</h1>
+            {comments}
         </div>
     )
 }
---
+++ src/data/CommentsData.js
@@ -0,0 +1,24 @@
+export const CommentsData = [
+    {
+        name: 'John Doe',
+        posted_on: "3/12/2020",
+        comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam tincidunt, nisl nisl aliquam mauris, et ultricies nisl nunc vel mauris. Sed euismod, nunc ut aliquam tincidunt, nisl nisl aliquam mauris, et ultricies nisl nunc vel mauris.",
+        replies: [
+            {
+                name: 'John Doe 2',
+                posted_on: "3/12/2020",
+                comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam tincidunt, nisl nisl aliquam mauris, et ultricies nisl nunc vel mauris. Sed euismod, nunc ut aliquam tincidunt, nisl nisl aliquam mauris, et ultricies nisl nunc vel mauris.",
+            },
+            {
+                name: 'John Doe 3',
+                posted_on: "3/12/2020",
+                comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam tincidunt, nisl nisl aliquam mauris, et ultricies nisl nunc vel mauris. Sed euismod, nunc ut aliquam tincidunt, nisl nisl aliquam mauris, et ultricies nisl nunc vel mauris.",
+            }
+        ],
+    },
+    {
+        name: 'John Doe 4',
+        posted_on: "3/12/2020",
+        comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam tincidunt, nisl nisl aliquam mauris, et ultricies nisl nunc vel mauris. Sed euismod, nunc ut aliquam tincidunt, nisl nisl aliquam mauris, et ultricies nisl nunc vel mauris.",
+    },
+];
